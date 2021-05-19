e48b6b3bb9edc25066d26872955e38596a30f121
Tobias Fricke <tobias.fricke@student.fh-kiel.de>
2021-05-19 09:56:12 +0200

    Test

---
+++ RestLib/test.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Tobia on 19.05.2021.
+//
+
+#include "test.hpp"
---
+++ RestLib/test.hpp
@@ -0,0 +1,14 @@
+//
+// Created by Tobia on 19.05.2021.
+//
+
+#ifndef FINAL_PROJECT_TEST_HPP
+#define FINAL_PROJECT_TEST_HPP
+
+
+class test {
+
+};
+
+
+#endif //FINAL_PROJECT_TEST_HPP
