08239563bf4b7101cf749198a7e9f99ec440be25
Tobias Fricke <tobias.fricke@student.fh-kiel.de>
2021-05-28 10:08:13 +0200

    OrderTab init

--- RestLib/order.cpp
+++ RestLib/order.cpp
@@ -4,17 +4,43 @@
 
 #include "order.hpp"
 #include <iostream>
+#include <ctime>
 #include <utility>
+#include <vector>
+#include <sstream>
+#include "Customer.hpp"
 
-RestLib::order::order(std::string _orderdate, std::string _ordername) : orderdate{std::move(_orderdate)} , ordername {std::move(_ordername)} {
+using namespace std;
+using namespace RestLib;
+
+order::order(string _orderdate, string _ordername) : orderdate{move(_orderdate)} , ordername {move(_ordername)} {
 
 }
 
-std::string RestLib::order::getOrderName() {
+string order::getOrderName() {
     return this->ordername;
 }
 
-std::string RestLib::order::getOrderDate() {
+string order::getOrderDate() {
     return this->orderdate;
 }
 
+void order::createNewOrder(const string &customerName, const string &dishName, const std::string& drinkName) {
+    // Get current date
+    time_t t = time(0);
+    tm* now = localtime(&t);
+    ostringstream stringStream;
+    stringStream << now->tm_mday << "." << (now->tm_mon + 1) << "." << (now->tm_year + 1900);
+    string orderDate = stringStream.str();
+
+    // Create new dish order
+    if ("No Dish" != dishName) {
+        order newDish(orderDate, dishName);
+        cout << "Customer: " << customerName << " ordered Dish: " << newDish.getOrderName() << " [" << newDish.getOrderDate() << "]" << endl;
+    }
+    // Create new drink order
+    if ("No Drink" != drinkName) {
+        order newDrink(orderDate, drinkName);
+        cout << "Customer: " << customerName << " ordered Drink: " << newDrink.getOrderName() << " [" << newDrink.getOrderDate() << "]" << endl;
+    }
+}
