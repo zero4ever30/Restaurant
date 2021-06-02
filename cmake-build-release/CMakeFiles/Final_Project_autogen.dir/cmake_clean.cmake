70399bb5659fb2f117d13255c0a5444ef5da61a5
TobiasF92 <84439060+TobiasF92@users.noreply.github.com>
2021-06-02 09:51:01 +0200

    Delete cmake-build-release directory

--- cmake-build-release/CMakeFiles/Final_Project_autogen.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/Final_Project_autogen"
-  "Final_Project_autogen/mocs_compilation.cpp"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/Final_Project_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
